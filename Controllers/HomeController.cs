ac22c061da150c5a2537a22a00e6e2e5cc40f9ba
Wheatley <30753183+hanlim83@users.noreply.github.com>
2019-11-13 10:22:09 +0800

    Added Basic Authentication Flow

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,9 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using OpsSecProject.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace OpsSecProject.Controllers
 {
-    [Authorize]
     public class HomeController : Controller
     {
-        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
@@ -17,19 +12,6 @@ namespace OpsSecProject.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        public IActionResult Claims()
-        {
-            ViewData["User"] = HttpContext.User;
-            return View();
-        }
-
-        [AllowAnonymous]
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
