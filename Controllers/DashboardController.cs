566f6ae638c5a797ad4949f6e13f4e47e24dc2cf
k553 <56666637+k553@users.noreply.github.com>
2019-11-14 15:51:24 +0800

    Added samp[e charts

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OpsSecProject.Models;
+
+namespace OpsSecProject.Controllers
+{
+    public class DashboardController : Controller
+    {
+
+        public IActionResult Bar()
+        {
+            Random rnd = new Random();
+
+            //list of department  
+            var lstModel = new List<ChartsViewModel>();
+            lstModel.Add(new ChartsViewModel
+            {
+                DimensionOne = "Technology",
+                Quantity = rnd.Next(10)
+            });
+            lstModel.Add(new ChartsViewModel
+            {
+                DimensionOne = "Sales",
+                Quantity = rnd.Next(10)
+            });
+            lstModel.Add(new ChartsViewModel
+            {
+                DimensionOne = "Marketing",
+                Quantity = rnd.Next(10)
+            });
+            lstModel.Add(new ChartsViewModel
+            {
+                DimensionOne = "Human Resource",
+                Quantity = rnd.Next(10)
+            });
+            lstModel.Add(new ChartsViewModel
+            {
+                DimensionOne = "Research and Development",
+                Quantity = rnd.Next(10)
+            });
+            lstModel.Add(new ChartsViewModel
+            {
+                DimensionOne = "Acconting",
+                Quantity = rnd.Next(10)
+            });
+            lstModel.Add(new ChartsViewModel
+            {
+                DimensionOne = "Support",
+                Quantity = rnd.Next(10)
+            });
+            lstModel.Add(new ChartsViewModel
+            {
+                DimensionOne = "Logistics",
+                Quantity = rnd.Next(10)
+            });
+
+            return View(lstModel);
+
+        }
+
+        public IActionResult Line()
+        {
+            Random rnd = new Random();
+
+            //list of countries  
+            var lstModel = new List<ChartsViewModel>();
+            lstModel.Add(new ChartsViewModel
+            {
+                DimensionOne = "Brazil",
+                Quantity = rnd.Next(10)
+            });
+            lstModel.Add(new ChartsViewModel
+            {
+                DimensionOne = "USA",
+                Quantity = rnd.Next(10)
+            });
+            lstModel.Add(new ChartsViewModel
+            {
+                DimensionOne = "Ier",
+                Quantity = rnd.Next(10)
+            });
+            lstModel.Add(new ChartsViewModel
+            {
+                DimensionOne = "gfg",
+                Quantity = rnd.Next(10)
+            });
+            lstModel.Add(new ChartsViewModel
+            {
+                DimensionOne = "ghA",
+                Quantity = rnd.Next(10)
+            });
+
+            return View(lstModel);
+        }
+    }
+}
