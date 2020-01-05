df8f657ecbb89bb8e35a1d8cfd40743323056efc
Wheatley <30753183+hanlim83@users.noreply.github.com>
2020-01-05 20:23:00 +0800

    Redesigned Login Process and updated packages

--- Controllers/LandingController.cs
+++ Controllers/LandingController.cs
@@ -3,7 +3,12 @@ using Microsoft.AspNetCore.Authentication.AzureAD.UI;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using OpsSecProject.Data;
+using OpsSecProject.Helpers;
 using OpsSecProject.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Activity = System.Diagnostics.Activity;
 
 namespace OpsSecProject.Controllers
@@ -11,17 +16,79 @@ namespace OpsSecProject.Controllers
     [AllowAnonymous]
     public class LandingController : Controller
     {
+        private readonly AccountContext _context;
+
+        public LandingController(AccountContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             if (User.Identity.IsAuthenticated)
                 return RedirectToAction("Index", "Home");
             else
-                return RedirectToAction("Login");
+                return RedirectToAction("RealmDiscovery");
+        }
+
+        public IActionResult RealmDiscovery(string ReturnUrl)
+        {
+            if (ReturnUrl != null)
+                ViewData["ReturnURL"] = ReturnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RealmDiscovery([Bind("Username", "recaptchaResponse", "ReturnUrl")]RealmDiscoveryModel user)
+        {
+            if (await GoogleRecaptchaHelper.IsReCaptchaV2PassedAsync(user.recaptchaResponse))
+            {
+                User identity = _context.Users.Where(u => u.Username == user.Username).FirstOrDefault();
+                if (identity == null)
+                {
+                    if (user.Username.Contains("@"))
+                    {
+                        var authenticationProperties = new AuthenticationProperties();
+                        authenticationProperties.Items["login_hint"] = user.Username;
+                        if (user.ReturnUrl == null)
+                            authenticationProperties.RedirectUri = "/Landing/";
+                        else
+                            authenticationProperties.RedirectUri = user.ReturnUrl;
+                        return Challenge(authenticationProperties, AzureADDefaults.AuthenticationScheme);
+                    } else
+                    {
+                        ViewData["Alert"] = "Danger";
+                        ViewData["Message"] = "We can't find an account with that Username. Please double check with your entered Usernmae?";
+                        return View(user);
+                    }
+                } else if (identity.Existence != Existence.Internal)
+                {
+                    var authenticationProperties = new AuthenticationProperties();
+                    authenticationProperties.Items["login_hint"] = user.Username;
+                    if (user.ReturnUrl == null)
+                        authenticationProperties.RedirectUri = "/Landing/";
+                    else
+                        authenticationProperties.RedirectUri = user.ReturnUrl;
+                    return Challenge(authenticationProperties, AzureADDefaults.AuthenticationScheme);
+                }
+                else
+                {
+                    TempData["Username"] = identity.Username;
+                    TempData["ReturnUrl"] = user.ReturnUrl;
+                    return Redirect("/Internal/Account/SignIn");
+                }
+            }
+            else
+            {
+                ViewData["Alert"] = "Warning";
+                ViewData["Message"] = "Unable to verify reCAPTCHA! Please try again";
+                return View(user);
+            }
         }
 
         public IActionResult Login(string ReturnUrl)
         {
-            if (ReturnUrl != null)
+            if (!String.IsNullOrEmpty(ReturnUrl))
                 ViewData["ReturnURL"] = ReturnUrl;
             return View();
         }
@@ -46,7 +113,7 @@ namespace OpsSecProject.Controllers
             var authenticationProperties = new AuthenticationProperties();
             authenticationProperties.Items["prompt"] = "login";
             authenticationProperties.RedirectUri = "/Landing/";
-            return Challenge(authenticationProperties,AzureADDefaults.AuthenticationScheme);
+            return Challenge(authenticationProperties, AzureADDefaults.AuthenticationScheme);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
