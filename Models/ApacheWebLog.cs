5a50c83181afeeda4a1398219cc2c1011b8c6ad7
Wheatley <30753183+hanlim83@users.noreply.github.com>
2020-01-29 20:46:21 +0800

    Add Dominic Codes for preliminary implment (#39)
    * Dom 21/1/20 (#38)
    
    * Updated Buckets to be private by default
    
    * Changes to LogContext Entities
    
    * Redefined LogContext DB classes 2
    
    * Refinements to SageMaker Testing
    
    * Pushed changes for weekly PR
    
    Co-authored-by: Wheatley <30753183+hanlim83@users.noreply.github.com>
    
    * 29/1/2020
    
    Co-authored-by: k553 <56666637+k553@users.noreply.github.com>

---
+++ Models/ApacheWebLog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpsSecProject.Models
+{
+    public class ApacheWebLog
+    {
+        public string host { get; set; }
+        public string ident { get; set; }
+        public string authuser { get; set; }
+        public string request { get; set; }
+        public string response { get; set; }
+        public int bytes { get; set; }
+        public int referer { get; set; }
+        public string agent { get; set; }
+
+        public DateTime datetime { get; set; }
+
+    }
+}
---
+++ Models/SSHServerLogs.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+//namespace OpsSecProject.Models
+//{
+//    public class SSHServerLogs
+//    {
+//        public string weekday { get; set; }
+//        public string month { get; set; }
+//        public string day { get; set; }
+//        public string year { get; set; }
+//        public string time { get; set; }
+//        public int host { get; set; }
+//        public int process { get; set; }
+//        public string identifier { get; set; }
+
+//        public DateTime message { get; set; }
+
+//    }
+//}
