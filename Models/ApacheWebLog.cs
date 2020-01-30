59ca7ec4be427f93171b3ce126d06ba90e0d0fe7
Wheatley <30753183+hanlim83@users.noreply.github.com>
2020-01-30 15:16:59 +0800

    PR after preliminary review (#41)
    * Updated Buckets to be private by default
    
    * Changes to LogContext Entities
    
    * Redefined LogContext DB classes 2
    
    * Refinements to SageMaker Testing
    
    * Pushed changes for weekly PR
    
    * Week 15 PR to own branch (#37)
    
    * Updated Buckets to be private by default
    
    * Changes to LogContext Entities
    
    * Redefined LogContext DB classes 2
    
    * Refinements to SageMaker Testing
    
    * Pushed changes for weekly PR
    
    * Dom 21/1/20 (#38)
    
    * Updated Buckets to be private by default
    
    * Changes to LogContext Entities
    
    * Redefined LogContext DB classes 2
    
    * Refinements to SageMaker Testing
    
    * Pushed changes for weekly PR
    
    Co-authored-by: Wheatley <30753183+hanlim83@users.noreply.github.com>
    
    * Weekly Commit
    
    * Change input names
    
    * 29/1/2020
    
    * Changed Glue Connections
    
    * Add Dominic Codes for preliminary implment (#39)
    
    * Dom 21/1/20 (#38)
    
    * Updated Buckets to be private by default
    
    * Changes to LogContext Entities
    
    * Redefined LogContext DB classes 2
    
    * Refinements to SageMaker Testing
    
    * Pushed changes for weekly PR
    
    Co-authored-by: Wheatley <30753183+hanlim83@users.noreply.github.com>
    
    * 29/1/2020
    
    Co-authored-by: k553 <56666637+k553@users.noreply.github.com>
    
    * Added Common Log Viewing Page
    
    * Changes for PR
    
    Co-authored-by: k553 <56666637+k553@users.noreply.github.com>

---
+++ Models/ApacheWebLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpsSecProject.Models
+{
+    public class ApacheWebLog
+    {
+        public string host { get; set; }
+        public string ident { get; set; }
+        public string authuser { get; set; }
+        public string datetime { get; set; }
+        public string request { get; set; }
+        public string response { get; set; }
+        public int bytes { get; set; }
+        public string referer { get; set; }
+        public string agent { get; set; }
+    }
+}
